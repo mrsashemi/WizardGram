a717718154a32c601e2c30983591cc65cbe4bf3d
mrsashemi <mrsashemi@gmail.com>
2022-11-12 18:03:15 -0800

    Add function and triggers to sql db

--- databases/db.sql
+++ databases/db.sql
@@ -22,7 +22,7 @@ CREATE TABLE blog_posts(
     title TEXT NOT NULL,
     body TEXT NOT NULL,
     photos TEXT[], 
-    user_id INT NOT NULL,
+    users_id INT NOT NULL,
     date_created DATE NOT NULL DEFAULT CURRENT_DATE,
     date_updated DATE NOT NULL DEFAULT CURRENT_DATE,
     CONSTRAINT fk_theme
@@ -30,7 +30,7 @@ CREATE TABLE blog_posts(
             REFERENCES themes(id)
             ON DELETE CASCADE,
     CONSTRAINT fk_user
-        FOREIGN KEY(user_id)
+        FOREIGN KEY(users_id)
             REFERENCES users(id)
             ON DELETE CASCADE
 );
@@ -40,5 +40,75 @@ CREATE TABLE blog_themes(
     theme_name NVARCHAR(50) NOT NULL UNIQUE
 );
 
+CREATE TABLE comments(
+    id SERIAL PRIMARY KEY,
+    body NVARCHAR(500) NOT NULL,
+    users_id INT NOT NULL,
+    blog_id INT NOT NULL,
+    parent_comment_id INT, 
+    date_created TIMESTAMPTZ DEFAULT NOW(),
+    date_updated TIMESTAMPTZ DEFAULT NOW(),
+    CONSTRAINT fk_user
+        FOREIGN KEY(users_id)
+            REFERENCES users(id)
+            ON DELETE CASCADE,
+    CONSTRAINT fk_blog
+        FOREIGN KEY(blog_id)
+            REFERENCES blog_posts(id)
+            ON DELETE CASCADE,
+    CONSTRAINT fk_parent_comment
+        FOREIGN KEY(parent_comment_id)
+            REFERENCES comments(id)
+            ON DELETE SET NULL
+);
 
+CREATE TABLE addresses(
+    id SERIAL PRIMARY KEY,
+    users_id INT NOT NULL,
+    city NVARCHAR(50) NOT NULL,
+    street NVARCHAR(25) NOT NULL,
+    state_code NVARCHAR(2) NOT NULL,
+    zip NVARCHAR(10) NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY(users_id)
+            REFERENCES users(id)
+            ON DELETE CASCADE
+);
 
+CREATE OR REPLACE FUNCTION UPDATE_DATE()
+    RETURNS TRIGGER
+    LANGUAGE PLPGSQL
+AS $$
+BEGIN 
+    NEW.date_updated = CURRENT_DATE;
+    RETURN NEW;
+END;
+$$;
+
+CREATE OR REPLACE FUNCTION UPDATE_TIMESTAMP()
+    RETURNS TRIGGER
+    LANGUAGE plpgsql
+AS $$
+BEGIN 
+    NEW.date_updated = NOW();
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER update_blog_date
+    BEFORE UPDATE 
+    ON blog_posts
+    FOR EACH ROW
+        EXECUTE PROCEDURE UPDATE_DATE();
+
+CREATE TRIGGER update_users_date
+    BEFORE UPDATE 
+    ON users
+    FOR EACH ROW
+        EXECUTE PROCEDURE UPDATE_DATE();
+
+CREATE TRIGGER update_users_date
+    BEFORE UPDATE 
+    ON users
+    FOR EACH ROW
+        EXECUTE PROCEDURE UPDATE_TIMESTAMP();
